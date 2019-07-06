32ada3406df2d266dc60b2bd8929b998a4edd4fa
Saad Saif <msaadsaif5@gmail.com>
2019-07-06 02:12:39 +0200

    add box plot of aggregate distances

---
+++ src/models/avgDistancesPerDegree.py
@@ -0,0 +1,10 @@
+import random
+
+class AvgDistancesPerDegree:
+    def __init__(self, avgDistances = None, temp = 0):
+        self.avgDistances = avgDistances
+        self.temp = temp
+
+    def getDummyData(self):        
+        dummyData = [AvgDistancesPerDegree([ i + 1 for i in range(random.randrange(5, 100)) ], random.randrange(-10, 30)) for i in range(1, 50)]
+        return dummyData
